46f78e304fdc6fb2c82579aed7321b8947cfbb0c
anilbathini1152 <95916329+anilbathini1152@users.noreply.github.com>
2022-03-21 20:59:17 +0530

    Update urls.py

--- pwdgen/urls.py
+++ pwdgen/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('',include('passwdgen.urls')),
     path('admin/', admin.site.urls),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+#     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+#     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
